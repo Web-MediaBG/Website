e8b6388df0275a5073f0e4219354af55156e8b6c
Web-Media <Web-Media@Bovis-grafica.be>
2021-07-02 00:23:49 +0200

    Versie 2/07/2021

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -18,7 +18,7 @@ menuClose.addEventListener("click", () => {
 let modal
 let number
 
-function TekstWillem(id){
+function Modal(id){
     modal = document.getElementById(id);
     modal.style.display = "block";
 }
@@ -32,34 +32,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-/*______________SLIDER______________*/
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
