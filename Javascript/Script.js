173a626166463fa7697c67478a19eb87d65f532e
Web-Media <Web-Media@Bovis-grafica.be>
2021-07-03 01:29:51 +0200

    Versie 3/07/2021

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -2,16 +2,54 @@ const doc = document;
 const menuOpen = doc.querySelector(".menu");
 const menuClose = doc.querySelector(".close");
 const overlay = doc.querySelector(".overlay");
+const overlay1 = doc.querySelector(".overlay1");
+const overlay2 = doc.querySelector(".overlay2");
+const Partners = doc.querySelector(".Partners");
+const Overons = doc.querySelector(".Overons");
+const Back1 = doc.querySelector(".Back1");
+const Back2 = doc.querySelector(".Back2");
+const nav = doc.querySelector(".nav");
+const Praesidium = doc.querySelector(".Praesidium");
+
 
 
 menuOpen.addEventListener("click", () => {
   overlay.classList.add("overlay--active");
+  nav.style.display = "none";
+
 });
 
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
+  nav.style.display = "";
 });
 
+Overons.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+  overlay1.classList.add("overlay--active");
+});
+
+Partners.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+  overlay2.classList.add("overlay--active");
+});
+
+Back1.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+  overlay1.classList.remove("overlay--active");
+});
+
+Back2.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+  overlay2.classList.remove("overlay--active");
+});
+
+Praesidium.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+  overlay1.classList.remove("overlay--active");
+  overlay2.classList.remove("overlay--active");
+  nav.style.display = "";
+});
 
 /*______________MODAL______________*/
 
